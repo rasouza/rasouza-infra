37066090780232da9eed3e27205a277182ca98bd
R. A. Souza <alves.wm@gmail.com>
2020-07-06 20:25:01 -0300

    updates config.js for frontend

--- diary/frontend/envs/config.js
+++ diary/frontend/envs/config.js
@@ -1,1 +1,10 @@
 window.USERS_URL='http://users.diary.localhost';
+window.JWKS = {
+  use: "sig",
+  kty: "RSA",
+  kid: "public:7cccb6b4-a6dc-4c3c-9a6c-0a1b4bbd44fb",
+  alg: "RS256",
+  n:
+    "v81bPqmDSdf_30MutQxDItxVIBCFM6PMU_0Nc3iWeCK-7gBoesykWjz1lCvkVRqJj9yCk-6aszbgSLIf4H3tGp9-LW43nSqpMjm1lVjCqvOOoc4sQFftZFgq-yuiGn3Af3mXy0AgvSjPH5vqvOz--bhWU8fYlcVAJl4gECZbqf8aXu3yKaE6IqA-UkGzHbYL20R1WzWAbY6g2lvrLHnBIsm-VAhyZmu3BtZhRGlB0iC9n5zfgbTqryDrUSnYtxLAEi-eVxUm-bo0PPrPHjPunO9fqZMnu7VITVkSI1Z2SWtzexTbl_vA6dNrjtOP9BLbQWGlohDvM2NHCY13AF_OPUEQQVFGFeh_uB6d6eekv3AGtseMMcjV0ly9nstCI7PjtpZ-MDwNga00cUDX33jPMzsW4eF0bESAQv2hyBvDXsIEuc-_aPI1t4Z9xv7FMxLlj7VDyp-DxSWwM9Bqmi9MGgOsIJJM3XnFtC_KeikGJJuKUqjMg4b0Ie5cjjA7glXqQZCq19SKPueXs2WB6aAP0CA3AaoWQoHtZZt5FceuKlwEs_DzAdWJ4WFBOsUTJuTOSWJu9pSZjdTbqcO2t5sDyL4Yn5adAwV_f0bfODOy9UPO76wRMCYk80ih-mePnVKeTn-oE9x2QNxJnj5dNqvbX5icfrrz3VwfK56PVACKSy8",
+  e: "AQAB",
+};
