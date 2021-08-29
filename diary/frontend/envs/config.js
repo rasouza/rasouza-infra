fb9c1739dce3fb4704352105ec2976f562b04f42
R. A. Souza <alves.wm@gmail.com>
2021-08-29 03:25:56 -0300

    add backend url to diary frontend

--- diary/frontend/envs/config.js
+++ diary/frontend/envs/config.js
@@ -2,4 +2,4 @@ window.ENABLE_LOGIN = true
 window.SUPABASE_URL = 'https://cggwvqtadgufjgzxreuq.supabase.co'
 window.SUPABASE_KEY =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyOTU4MTk0MSwiZXhwIjoxOTQ1MTU3OTQxfQ.dFX_AxjSbjy9o34c-al9msFtJxVtUPmX0ZzW3saxOxc'
-window.BASE_URL = 'https://diary.rasouza.dev'
+window.CORE_URL = 'https://diary-core.rasouza.dev'
